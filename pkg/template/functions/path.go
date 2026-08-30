601af14958fd0e973b3a7c5839b19fedce79faa5
Alexandr Konichenko <30230574+JFAexe@users.noreply.github.com>
2026-08-30 19:13:48 +0300

    feat: extend path functions

--- pkg/template/functions/path.go
+++ pkg/template/functions/path.go
@@ -2,9 +2,21 @@ package functions
 
 import (
 	"path"
+	"strings"
+
+	"github.com/bmatcuk/doublestar/v4"
 
 	"github.com/JFAexe/tem/pkg/convert"
 )
+
+type DirFile struct {
+	Dir  string `json:"dir"  yaml:"dir"  toml:"dir"`
+	File string `json:"file" yaml:"file" toml:"file"`
+}
+
+func (p DirFile) String() string {
+	return p.Dir + p.File
+}
 
 type Path struct{}
 
@@ -19,6 +31,26 @@ func (*Path) Clean(value any) string {
 func (*Path) IsAbs(value any) bool {
 	return path.IsAbs(convert.ToString(value))
 }
+
+func (*Path) Root(value any) string {
+	str := convert.ToString(value)
+	if str == "" {
+		return ""
+	}
+
+	for len(str) > 1 {
+		dir, _ := path.Split(str)
+
+		if dir == "" {
+			break
+		}
+
+		str = path.Clean(dir)
+	}
+
+	return str
+}
+
 func (*Path) Base(value any) string {
 	return path.Base(convert.ToString(value))
 }
@@ -31,16 +63,37 @@ func (*Path) Ext(value any) string {
 	return path.Ext(convert.ToString(value))
 }
 
+func (*Path) TrimExt(value any) string {
+	str := convert.ToString(value)
+	if str == "" {
+		return ""
+	}
+
+	var (
+		name = path.Base(str)
+		ext  = path.Ext(name)
+	)
+
+	if ext == "" || ext == name {
+		return str
+	}
+
+	return strings.TrimSuffix(str, ext)
+}
+
 func (*Path) Join(values ...any) string {
 	return path.Join(convert.ToStringSlice(values)...)
 }
 
-func (*Path) Split(value any) []string {
+func (*Path) Split(value any) DirFile {
 	dir, file := path.Split(convert.ToString(value))
 
-	return []string{dir, file}
+	return DirFile{
+		Dir:  dir,
+		File: file,
+	}
 }
 
 func (*Path) Match(pattern, name any) (bool, error) {
-	return path.Match(convert.ToString(pattern), convert.ToString(name))
+	return doublestar.Match(convert.ToString(pattern), convert.ToString(name))
 }
